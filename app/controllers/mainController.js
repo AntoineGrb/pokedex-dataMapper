8ca5b89b94ae2b8ec948838ce901b4dfe6e39827
AntoineGrb <antoine_grubert@hotmail.fr>
2023-10-12 16:48:13 +0200

    correction bugs et opti menu

--- app/controllers/mainController.js
+++ app/controllers/mainController.js
@@ -1,18 +1,23 @@
+const { render } = require('ejs');
 const dataMapper = require('../data/dataMapper')
 
 const mainController = {
 
     homePage: async (req , res) => {
         const pokemons = await dataMapper.getAllPokemons();        
-        res.render('index' , {pokemons});
+        res.render('index' , {pokemons, page:'home'});
     },
 
     pokemonPage: async (req , res) => {
         const id = req.params.id;
         const pokemon = await dataMapper.getPokemon(id); 
         const pokemonTypes = await dataMapper.getPokemonType(id);   
-        res.render('pokemonPage' , {pokemon:pokemon , types:pokemonTypes});
+        res.render('pokemonPage' , {pokemon:pokemon , types:pokemonTypes, page:'pokemon'});
     },
+
+    typesPage: (req, res) => {
+        res.render('types' , {page:'type'});
+    }
 
 }
 
--- app/router.js
+++ app/router.js
@@ -2,6 +2,7 @@ const router = require('express').Router();
 const mainController = require('./controllers/mainController');
 
 router.get('/' , mainController.homePage);
+router.get('/types' , mainController.typesPage);
 router.get('/pokemon/:id' , mainController.pokemonPage);
 
 module.exports = router;
